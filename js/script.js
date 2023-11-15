afa44a181ee01e487b046db6afdc9402bca66082
RicTieri <tieri.riccardo@gmail.com>
2023-11-15 17:30:12 +0100

    added ex dadi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-const playerOne = Math.floor(Math.random * 6) + 1
-const pcGame = Math.floor(Math.random * 6) + 1
+const playerOne = Math.floor(Math.random() * 6) +1
+const pcGame = Math.floor(Math.random() * 6) + 1
 
-console.log(playerOne)
 
 if (playerOne > pcGame) {
-  console.log(playerOne, pcGame, 'the winner is the Player')
+  console.log(playerOne, pcGame, 'The winner is the Player')
 } else if (pcGame > playerOne){
-  console.log(playerOne, pcGame, 'the winner is the PC')
+  console.log(playerOne, pcGame, 'The winner is the PC')
+} else {
+  console.log(playerOne, pcGame, 'Draw')
 }
